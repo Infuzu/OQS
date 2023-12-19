62ed10de9a563e257ce3bc3d42a4727b22d4c8e2
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-12-19 17:45:48 -0500

    adding basic functionality

--- python_oqs_implementation/engine.py
+++ python_oqs_implementation/engine.py
@@ -24,17 +24,54 @@ class OQSBaseError(Exception):
         self.readable_name: str = readable_name
 
 
-class InvalidArgumentQuantity(OQSBaseError):
+class OQSInvalidArgumentQuantityError(OQSBaseError):
     def __init__(self, function_name: str, expected_min: int, expected_max: int, actual: int) -> None:
         message: str = (
             f"Function '{function_name}' expected at least {expected_min} with a max of {expected_max} arguments, "
             f"but got {actual}"
         )
-        super().__init__(readable_name="Invalid Argument Quantity", message=message)
+        super().__init__(readable_name="Invalid Argument Quantity Error", message=message)
+
+
+class OQSSyntaxError(OQSBaseError):
+    def __init__(self, message: str) -> None:
+        super().__init__(readable_name="Syntax Error", message=message)
+
+
+class OQSTypeError(OQSBaseError):
+    def __init__(self, message: str) -> None:
+        super().__init__(readable_name="Type Error", message=message)
+
+
+class OQSUndefinedVariableError(OQSBaseError):
+    def __init__(self, variable_name: str) -> None:
+        message: str = f"The variable {variable_name} is not defined."
+        super().__init__(readable_name="Undefined Variable Error", message=message)
+
+
+class OQSUndefinedFunctionError(OQSBaseError):
+    def __init__(self, function_name: str) -> None:
+        message: str = f"The function '{function_name}' is not a valid function."
+        super().__init__(readable_name="Undefined Function Error", message=message)
+
+
+class OQSFunctionEvaluationError(OQSBaseError):
+    def __init__(self, function_name: str, message: str) -> None:
+        full_message: str = f"Error in function '{function_name}': {message}"
+        super().__init__(readable_name="Function Evaluation Error", message=full_message)
 
 
 class ASTNode:
     pass
+
+
+class NullNode(ASTNode):
+    pass
+
+
+class UnevaluatedNode(ASTNode):
+    def __init__(self, token: str) -> None:
+        self.token: str = token
 
 
 class BinaryOpNode(ASTNode):
@@ -65,8 +102,8 @@ class ListNode(ASTNode):
 
 
 class KVSNode(ASTNode):
-    def __init__(self, key_value_store: dict[str, any]) -> None:
-        self.key_value_store: dict[str, any] = key_value_store
+    def __init__(self, key_value_store: dict[ASTNode, ASTNode]) -> None:
+        self.key_value_store: dict[ASTNode, ASTNode] = key_value_store
 
 
 class BooleanNode(ASTNode):
@@ -82,10 +119,55 @@ class FunctionNode(ASTNode):
 
 class OQSParser:
     def parse(self, expression: str) -> ASTNode:
-        tokens: list[str] = re.findall(r'\b\w+\([^()]*\((?:[^\(\)]|(?R))*\)[^()]*\)|"[^"]*"|\S+', expression)
+        tokens: list[str] = self.tokenize_expression(expression=expression)
         return self.parse_expression(tokens=tokens)
 
-    def parse_expression(self, tokens: list[str], pos: int = 0) -> ASTNode:
+    @staticmethod
+    def tokenize_expression(expression: str) -> list[str]:
+        try:
+            tokens: list[str] = []
+            current_token: list[str] = []
+            level: int = 0
+            i: int = 0
+            while i < len(expression):
+                char: str = expression[i]
+                if char in ['"', "'"]:
+                    string_char: str = char
+                    current_token.append(char)
+                    i += 1
+                    while i < len(expression) and expression[i] != string_char:
+                        current_token.append(expression[i])
+                        i += 1
+                    if i < len(expression):
+                        current_token.append(expression[i])
+                    i += 1
+                elif char in ['(', '{', '[']:
+                    level += 1
+                    current_token.append(char)
+                    i += 1
+                elif char in [')', '}', ']']:
+                    level -= 1
+                    current_token.append(char)
+                    i += 1
+                    if level == 0:
+                        tokens.append(''.join(current_token))
+                        current_token: list[str] = []
+                elif char == ',' and level == 0:
+                    if current_token:
+                        tokens.append(''.join(current_token))
+                        current_token: list[str] = []
+                    i += 1
+                else:
+                    current_token.append(char)
+                    i += 1
+            if current_token:
+                tokens.append(''.join(current_token))
+            tokens: list[str] = [token.strip() for token in tokens]
+            return tokens
+        except Exception as e:
+            raise OQSSyntaxError(f"Invalid syntax: {str(e)}")
+
+    def parse_expression(self, tokens: list[str], pos: int = 0) -> ASTNode | type(None):
         left: ASTNode = self.parse_term(tokens=tokens, pos=pos)
         pos += 1
         if pos < len(tokens):
@@ -101,10 +183,12 @@ class OQSParser:
             return NumberNode(value=int(token))
         elif token in ['true', 'false']:
             return BooleanNode(value=token == 'true')
+        elif token == 'null':
+            return NullNode()
         elif token.startswith('"') and token.endswith('"') or token.startswith("'") and token.endswith("'"):
             return StringNode(value=token[1:-1])
         elif token.startswith('{') and token.endswith('}'):
-            kvs: dict[str, ASTNode] = self.parse_kvs(token[1:-1])
+            kvs: dict[ASTNode, ASTNode] = self.parse_kvs(token[1:-1])
             return KVSNode(key_value_store=kvs)
         elif token.startswith('[') and token.endswith(']'):
             elements: list[ASTNode] = self.parse_list(args_str=token[1:-1])
@@ -116,69 +200,55 @@ class OQSParser:
 
     def parse_function_call(self, token: str) -> FunctionNode:
         function_name, args_str = token[:-1].split('(', 1)
-        args_tokens = self.tokenize_arguments(args_str)
-        args = [self.parse_expression([arg], 0) for arg in args_tokens]
+        args_tokens: list[str] = self.tokenize_expression(expression=args_str)
+        args: list[ASTNode] = [UnevaluatedNode(token=arg) for arg in args_tokens]
         return FunctionNode(name=function_name, args=args)
 
     def parse_list(self, args_str: str) -> list[ASTNode]:
         if not args_str.strip():
             return []
-        args_tokens: list[str] = self.tokenize_arguments(args_str=args_str)
+        args_tokens: list[str] = self.tokenize_expression(expression=args_str)
         return [self.parse_expression(tokens=[token], pos=0) for token in args_tokens]
 
-    def parse_kvs(self, kvs_str: str) -> dict[str, ASTNode]:
-        kvs_tokens: list[str] = self.tokenize_arguments(kvs_str)
-        kvs: dict[str, ASTNode] = {}
+    def parse_kvs(self, kvs_str: str) -> dict[ASTNode, ASTNode]:
+        kvs_tokens: list[str] = self.tokenize_expression(expression=kvs_str)
+        kvs: dict[ASTNode, ASTNode] = {}
 
         for token in kvs_tokens:
-            if ':' not in token:
-                raise ValueError(f"Invalid key-value pair: {token}")
-            key, value = token.split(':', 1)
-            key: str = key.strip()
-            value: str = value.strip()
-            kvs[key] = self.parse_expression([value], 0)
+            key, value = self.split_key_value_pair(token)
+            kvs[self.parse_expression([key], 0)] = self.parse_expression([value], 0)
 
         return kvs
 
     @staticmethod
-    def tokenize_arguments(args_str: str) -> list[str]:
-        tokens = []
-        current_token = []
-        level = 0
-        i = 0
-        in_string = False
-        string_char = ''
+    def split_key_value_pair(token: str) -> tuple[str, str]:
+        colon_index: int = -1
+        level: int = 0
+        in_string: bool = False
+        string_char: str = ''
 
-        while i < len(args_str):
-            char = args_str[i]
-
+        for i, char in enumerate(token):
             if char in ['"', "'"]:
-                if not in_string:
-                    in_string = True
-                    string_char = char
-                elif char == string_char:
-                    in_string = False
-                current_token.append(char)
+                if in_string and char == string_char:
+                    in_string: bool = False
+                elif not in_string:
+                    in_string: bool = True
+                    string_char: str = char
+            elif char == ":" and level == 0 and not in_string:
+                colon_index: int = i
+                break
+            elif char in ['{', '[', '(']:
+                level += 1
+            elif char in ['}', '[', ')']:
+                level -= 1
 
-            elif not in_string:
-                if char == ',' and level == 0:
-                    tokens.append(''.join(current_token).strip())
-                    current_token = []
-                else:
-                    if char in ['(', '[', '{']:
-                        level += 1
-                    elif char in [')', ']', '}']:
-                        level -= 1
-                    current_token.append(char)
-            else:
-                current_token.append(char)
+        if colon_index == -1:
+            raise OQSSyntaxError(F"Invalid KVS pair {token}")
 
-            i += 1
+        key: str = token[:colon_index].strip()
+        value: str = token[colon_index + 1:].strip()
 
-        if current_token:
-            tokens.append(''.join(current_token).strip())
-
-        return tokens
+        return key, value
 
 
 class OQSInterpreter:
@@ -192,26 +262,35 @@ class OQSInterpreter:
         }
 
     def evaluate_function(self, node: FunctionNode) -> any:
-        if node.name == 'ADD':
-            print(node.args)
-            print(node.args[0].name)
-            if len(node.args) < 2:
-                raise InvalidArgumentQuantity(
-                    function_name=node.name, expected_min=2, expected_max=1000000, actual=len(node.args)
-                )
-            return sum([self.evaluate(arg) for arg in node.args])
-        else:
-            raise ValueError("Unknown Function")
+        try:
+            if node.name == 'ADD':
+                if len(node.args) < 2:
+                    raise OQSInvalidArgumentQuantityError(
+                        function_name=node.name, expected_min=2, expected_max=1000000, actual=len(node.args)
+                    )
+                evaluated_args: list[ASTNode] = [self.evaluate(arg) for arg in node.args]
+                return sum(evaluated_args)
+            else:
+                raise OQSUndefinedFunctionError(node.name)
+        except OQSBaseError:
+            raise
+        except Exception as e:
+            raise OQSFunctionEvaluationError(function_name=node.name, message=str(e))
 
     def evaluate(self, node: ASTNode) -> any:
         if isinstance(node, NumberNode):
             return node.value
         elif isinstance(node, StringNode):
             return node.value
+        elif isinstance(node, NullNode):
+            return None
         elif isinstance(node, ListNode):
             return [self.evaluate(elem) for elem in node.elements]
         elif isinstance(node, VariableNode):
-            return self.variables.get(node.name, None)
+            if node.name in self.variables:
+                return self.variables[node.name]
+            else:
+                raise OQSUndefinedVariableError(node.name)
         elif isinstance(node, BinaryOpNode):
             left: any = self.evaluate(node.left)
             right: any = self.evaluate(node.right)
@@ -219,11 +298,13 @@ class OQSInterpreter:
         elif isinstance(node, FunctionNode):
             return self.evaluate_function(node)
         elif isinstance(node, KVSNode):
-            return {key: self.evaluate(value) for key, value in node.key_value_store}
+            return {self.evaluate(key): self.evaluate(value) for key, value in node.key_value_store.items()}
         elif isinstance(node, BooleanNode):
             return node.value
+        elif isinstance(node, UnevaluatedNode):
+            return self.evaluate(OQSParser().parse_expression([node.token], 0))
         else:
-            raise ValueError("Unknown node")
+            raise OQSSyntaxError(f"Unable to parse the following: {node}")
 
     def set_variables(self, variable_map: dict[str, any]) -> None:
         self.variables: dict[str, any] = variable_map
@@ -266,7 +347,7 @@ def oqs_engine(
 
 
 def main():
-    expression: str = "ADD(ADD(3, 5), 2)"
+    expression: str = "SUB(2, 5, 6)"
     variables: dict[str, any] = {}
     string_embedded: bool = False
     result: dict[str, any] = oqs_engine(expression=expression, variables=variables, string_embedded=string_embedded)
