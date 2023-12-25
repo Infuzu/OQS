f11eff83773c76af96f611cf203d26286bbbf6ad
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-12-24 19:34:44 -0500

    adding package setup

---
+++ python_oqs_implementation/setup.py
@@ -0,0 +1,28 @@
+from setuptools import (setup, find_packages)
+
+
+setup(
+    name='oqs',
+    version='0.4.1',
+    packages=find_packages(include=['oqs', 'oqs.*']),
+    description=
+    "OQS (Open Quick Script) is a Python implementation of a versatile expression language optimized for processing "
+    "fundamental types and operations. It allows for interpreting expressions, optionally with variables in the form "
+    "of dictionaries, maps, or JSON, to deliver consistent and logical results. Adhering to the OQS Specification, "
+    "this implementation supports a variety of features including basic and advanced operations, custom functions, "
+    "performance monitoring, and extensive error handling. Ideal for a wide range of applications, OQS simplifies "
+    "expression evaluation and enhances Python's computational capabilities.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Infuzu',
+    author_email='oqs@infuzu.com',
+    url='https://github.com/Infuzu/OQS',
+    license='Creative Commons Attribution 4.0 International License',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Creative Commons Attribution 4.0 International License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    license_files=('../LICENSE.md', )
+)
